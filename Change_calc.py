68ec4197d326c87faeb63e9f155a5a32d7cdf88c
mdh111 <mikeyhill@gmail.com>
2019-05-24 15:20:25 +0100

    Change calculator

---
+++ Change_calc.py
@@ -0,0 +1,35 @@
+billAmount=int(input("Enter bill amount:"))
+paidAmount=int(input("Enter paid amount:"))
+balance=paidAmount-billAmount
+
+print("Balance:", balance)
+
+fifty=int(balance/50)
+if fifty>0:
+    print("£50:",fifty)
+    balance=balance-(fifty*50)
+
+twenty=int(balance/20)
+if twenty>0:
+    print("£20:",twenty)
+    balance=balance-(twenty*20)
+
+ten=int(balance/10)
+if ten>0:
+    print("£10:",ten)
+    balance=balance-(ten*10)
+
+five=int(balance/5)
+if five>0:
+    print("£5:",five)
+    balance=balance-(five*5)
+
+two=int(balance/2)
+if two>0:
+    print("£2:",two)
+    balance=balance-(two*2)
+
+one=int(balance/1)
+if one>0:
+    print("£1:",one)
+    balance=balance-(one*1)
