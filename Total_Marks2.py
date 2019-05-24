39608fa3ab670ed3c4107e78b53c0608c7b1970e
mdh111 <mikeyhill@gmail.com>
2019-05-24 09:58:28 +0100

    Calculate total marks and percetage

---
+++ Total_Marks2.py
@@ -0,0 +1,12 @@
+physics=45
+chemistry=59
+math=32
+totalMarks=physics+chemistry+math
+totalPercentage=totalMarks*100/150
+print("Physics: ",physics)
+print("Chemistry: ",chemistry)
+print("Maths: ",math)
+print("-----------------")
+print("Total Marks: ",totalMarks)
+print("Total: ",totalPercentage)
+print("-----------------")
