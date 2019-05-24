4a0f3ae87609ca453fcbdd164d34f0d67be56ffa
mdh111 <mikeyhill@gmail.com>
2019-05-24 11:27:42 +0100

    using user input to calculate

--- Total_Marks_Variable.py
+++ Total_Marks_Variable.py
@@ -1,7 +1,7 @@
-physics=45
-chemistry=59
-math=32
-totalMarks=physics+chemistry+math
+physics=input("Physics mark:")
+chemistry=input("Chemistry mark:")
+math=input("Maths mark:")
+totalMarks=int(physics)+int(chemistry)+int(math)
 totalPercentage=totalMarks*100/150
 print("Physics: ",physics)
 print("Chemistry: ",chemistry)
