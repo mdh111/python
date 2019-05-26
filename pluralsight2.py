6810e438e7bce587ec64b35f3aecfc4bf75b2dbb
mdh111 <mikeyhill@gmail.com>
2019-05-26 14:08:17 +0100

    pyhton INT, FLOAT and BOOL

---
+++ pluralsight2.py
@@ -0,0 +1,8 @@
+print("int to float",float(7))
+print("string to float",float("123"))
+print("int and float converts to float",int(2)+float(7))
+
+print(bool(0))  #bool 0 returns false
+print(bool(2))  #bool any number other than zero returns true
+print(bool("")) #bool an empty string returns false
+print(bool("Some text")) #bool a non-empty string returns true
