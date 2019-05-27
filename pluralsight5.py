198fc5a5a277569945572eda8e7fc13faaa8a21b
mdh111 <mikeyhill@gmail.com>
2019-05-27 10:32:17 +0100

    Lists

---
+++ pluralsight5.py
@@ -0,0 +1,10 @@
+shoppingList = []
+
+shoppingList.append("Apples")
+shoppingList.append("Milk")
+print(shoppingList)
+shoppingList[1] = "Bread"
+print(shoppingList)
+
+anotherList=list("characterlist")
+print(anotherList)
