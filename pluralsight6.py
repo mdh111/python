11100e54f18fcb04027c3c23cfbfb2b3bb8fc362
mdh111 <mikeyhill@gmail.com>
2019-05-27 10:42:23 +0100

    FOR loop and DICTIONARIES

--- pluralsight6.py
+++ pluralsight6.py
@@ -2,3 +2,6 @@ telNumbers = {"person1":12345,"person2":4325}
 print(telNumbers["person2"])
 telNumbers["person3"] = 55555
 print(telNumbers)
+
+for name in telNumbers:
+    print(name, telNumbers[name])
