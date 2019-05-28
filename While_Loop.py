fefab2a17bb13a5cdd9efedf34ff0ce888640410
mdh111 <mikeyhill@gmail.com>
2019-05-28 10:48:40 +0100

    WHILE. Times table with 2 variables

--- While_Loop.py
+++ While_Loop.py
@@ -8,3 +8,10 @@ a=1
 while a<=10:
     print(number,"x",a,"=",(number*a))
     a+=1
+
+number=int(input("Enter a number:"))
+number2=int(input("Enter another number:"))
+count=1
+while count<=number2:
+    print(number,"x",count,"=",(number*count))
+    count+=1
