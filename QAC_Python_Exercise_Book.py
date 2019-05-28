daf24522e723716992fc7eef40dc1912a240dd19
mdh111 <mikeyhill@gmail.com>
2019-05-28 15:42:03 +0100

    Conditionals 2

--- QAC_Python_Exercise_Book.py
+++ QAC_Python_Exercise_Book.py
@@ -5,7 +5,11 @@ number1=int(input("Enter a number:"))
 number2=int(input("Enter a second number:"))
 myBoolean=bool(int(input("Enter 1 to add and 0 to multiply:")))
 
-if myBoolean==True:
+if number1 == 0:
+    result=number2
+elif number2 == 0:
+    result=number1
+elif myBoolean==True:
     result=number1+number2
 else:
     result=number1*number2
