377f600c5ec7105680b348a9c43b6b600b97c4cb
mdh111 <mikeyhill@gmail.com>
2019-05-28 16:35:31 +0100

    More efficient searching by comparing the first character only and then comparing the whole string if the first character is found. Also skips over searching through the string that has already been found.

--- While_Loop_findWordInMessage.py
+++ While_Loop_findWordInMessage.py
@@ -3,7 +3,9 @@ what=input("What are you looking for?")
 i=0
 count=0
 while i<len(msg):
-    if (msg[i:len(what)+i]) == what:
-        count = count + 1
+    if msg[i] == what[0]:    
+        if (msg[i:len(what)+i]) == what:
+            count += 1
+            i = i + len(what)-1
     i += 1
 print(count)
