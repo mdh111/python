0f92d1f36208d76c36dbfedba73c59a045440cdc
mdh111 <mikeyhill@gmail.com>
2019-05-29 11:09:04 +0100

    Converting each character in a string

---
+++ ASCII_characterConversionString.py
@@ -0,0 +1,14 @@
+def change(message):
+    i=0
+    newMessage=""
+    while i < len(message):
+        if ord(message[i])>=65 and ord(message[i])<=90:
+            newMessage += chr(ord(message[i])+32)
+        elif ord(message[i])>=97 and ord(message[i])<=122:
+            newMessage += chr(ord(message[i])-32)
+        else:
+            newMessage += message[i]
+        i+=1
+    return newMessage
+
+print(change(input("Enter a message to convert:")))
