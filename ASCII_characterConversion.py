fccb39834969709f92adfcf3dbc3c1352bafe5ee
mdh111 <mikeyhill@gmail.com>
2019-05-29 10:35:53 +0100

    added case conversion

--- ASCII_characterConversion.py
+++ ASCII_characterConversion.py
@@ -8,3 +8,13 @@ elif b>=48 and b<=57:
     print("Number")
 else:
     print("Any other character")
+
+
+a = input("Enter a letter of the alphapet:")
+b = ord(a)    
+if b>=65 and b<=90:
+    print(chr(b+32))
+elif b>=97 and b<=122:
+    print(chr(b-32))
+else:
+    print("Not a letter of the alphabet")
