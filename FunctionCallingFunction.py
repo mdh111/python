9c53b987f78d865cbc8fe2cba8ec104fc4a2aac4
mdh111 <mikeyhill@gmail.com>
2019-05-31 10:16:39 +0100

    Function which calls another function

--- FunctionCallingFunction.py
+++ FunctionCallingFunction.py
@@ -1,13 +1,23 @@
-def addition(a,b):
+def add(a,b):
     c=a+b
     print("Addition result is ",c)
 
-def subtraction(a,b):
+def subtract(a,b):
     c=a-b
     print("Subtraction result is ",c)
+
+def multiply(a,b):
+    c=a*b
+    print("Multiplication result is ",c)
+
+def divide(a,b):
+    c=a/b
+    print("Division result is ",c)
 
 def operation(f,a,b):
     f(a,b)
 
-operation(addition,10,20)
-operation(subtraction,40,20)
+operation(add,10,20)
+operation(subtract,40,20)
+operation(divide,50,5)
+operation(multiply,3,8)
